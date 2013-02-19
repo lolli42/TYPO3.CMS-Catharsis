11e707238e382ef529d308b454f225c0eb56b46a
Christian Kuhn <lolli@schwarzbu.ch>
2013-02-19 20:19:18 +0100

    [TASK] Merge t3lib/thumbs.php to typo3/thumbs.php
    In order to get rid of the t3lib directory, the content
    of t3lib/thumbs.php is merged back to typo3/thumbs.php again.

--- typo3/thumbs.php
+++ typo3/thumbs.php
@@ -24,14 +24,26 @@
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
 /**
  * Generates a thumbnail and returns an image stream, either GIF/PNG or JPG
  *
  * @author René Fritz <r.fritz@colorcube.de>
  */
+
 // Set error reporting
 error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
 require 'init.php';
-// include thumbs script
-require PATH_t3lib . 'thumbs.php';
+
+/*
+ * @deprecated since 6.0, the classname SC_t3lib_thumbs and this file is obsolete
+ * and will be removed with 6.2. The class was renamed and is now located at:
+ * typo3/sysext/backend/Classes/View/ThumbnailView.php
+ */
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('backend') . 'Classes/View/ThumbnailView.php';
+
+// Make instance:
+$SOBE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\View\\ThumbnailView');
+$SOBE->init();
+$SOBE->main();
 ?>
