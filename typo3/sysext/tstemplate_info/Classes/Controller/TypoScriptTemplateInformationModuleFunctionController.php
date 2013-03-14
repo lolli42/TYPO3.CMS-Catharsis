32209681ab838c1e59f5a239873420aebc7dac97
Christian Kuhn <lolli@schwarzbu.ch>
2013-03-14 21:11:14 +0100

    [BUGFIX] Missing labels in backend modules
    There seems to be some magic in SOBE that automatically includes
    language files if they are located parallel to the module that is
    loaded. The task from #46143 and #46144 registered new wizard classes
    at a different location, so the magic doesn't hit anymore. As a
    result, no labels are show in above extensions in the backend.
    Fix is to include the locallang file in main() of the modules. The
    direct access to $LANG is removed along the way by accessing
    $GLOBALS['LANG'] directly.
    
    Change-Id: Iecddb181141f5317b87e405ada925c61cfd9261c
    Fixes: #46296
    Related: #46143
    Related: #46144
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/18925
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/sysext/tstemplate_info/Classes/Controller/TypoScriptTemplateInformationModuleFunctionController.php
+++ typo3/sysext/tstemplate_info/Classes/Controller/TypoScriptTemplateInformationModuleFunctionController.php
@@ -202,6 +202,7 @@ class TypoScriptTemplateInformationModuleFunctionController extends \TYPO3\CMS\B
 	public function main() {
 		global $BACK_PATH;
 		global $tmpl, $tplRow, $theConstants;
+		$GLOBALS['LANG']->includeLLFile('EXT:tstemplate_info/locallang.xlf');
 		$this->pObj->MOD_MENU['includeTypoScriptFileContent'] = TRUE;
 		$edit = $this->pObj->edit;
 		$e = $this->pObj->e;
