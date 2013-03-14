f6ff12433cf6fe89b25d443666641edcd21eabf4
Felix Kopp <felix-source@phorax.com>
2013-03-14 23:49:47 +0100

    [TASK] thumbs.php ease require/include path
    Currently typo3/thumbs.php requires t3lib/thumbs.php and
    t3lib/thumbs.php requires EXT:backend/Classes/View/ThumbnailView.php.
    
    Include ThumbnailView directly in typo3/thumbs.php.
    But keeps deprecation in place and other files untouched.
    
    Change-Id: I1c464e683cb08f674ac47cb87f296b185fd35db5
    Fixes: #46308
    Releases: 6.1
    Reviewed-on: https://review.typo3.org/18928
    Reviewed-by: Benjamin Mack
    Tested-by: Benjamin Mack
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

--- typo3/thumbs.php
+++ typo3/thumbs.php
@@ -35,13 +35,8 @@
 error_reporting(E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED));
 require 'init.php';
 
-/*
- * @deprecated since 6.0, the classname SC_t3lib_thumbs and this file is obsolete
- * and will be removed with 6.2. The class was renamed and is now located at:
- * typo3/sysext/backend/Classes/View/ThumbnailView.php
- */
+// include thumbs script
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('backend') . 'Classes/View/ThumbnailView.php';
-
 // Make instance:
 $SOBE = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Backend\\View\\ThumbnailView');
 $SOBE->init();
