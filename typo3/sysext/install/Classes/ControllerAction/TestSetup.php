410ac0484c674e59f49ad339c1d2e11b7e644660
Christian Kuhn <lolli@schwarzbu.ch>
2013-05-26 15:18:39 +0200

    [TASK][INSTALL] Implement test convert image formats

--- typo3/sysext/install/Classes/ControllerAction/TestSetup.php
+++ typo3/sysext/install/Classes/ControllerAction/TestSetup.php
@@ -32,12 +32,19 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class TestSetup extends AbstractAction implements ActionInterface {
 
 	/**
+	 * @var string Absolute path to image folder
+	 */
+	protected $imageBasePath = '';
+
+	/**
 	 * Handle this action
 	 *
 	 * @return string content
 	 */
 	public function handle() {
 		$this->initialize();
+
+		$this->imageBasePath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('install') . 'Resources/Public/Images/';
 
 		$actionMessages = array();
 		if (isset($this->postValues['set']['testMail'])) {
@@ -48,7 +55,22 @@ class TestSetup extends AbstractAction implements ActionInterface {
 			$actionMessages[] = $this->createTrueTypeFontDpiTestImage();
 		}
 
+		if (isset($this->postValues['set']['testConvertImageFormatsToJpg'])) {
+			$this->view->assign('convertImageFormatsToJpgTested', TRUE);
+			if ($this->isImageMagickEnabledAndConfigured()) {
+				$actionMessages[] = $this->convertImageFormatsToJpg();
+			} else {
+				/** @var \TYPO3\CMS\Install\Status\StatusInterface $message */
+				$message = $this->objectManager->get('TYPO3\\CMS\\Install\\Status\\ErrorStatus');
+				$message->setTitle('Test image formats tests not executed');
+				$message->setMessage('Image handling is disabled or not configured.');
+				$actionMessages[] = $message;
+			}
+		}
+
 		$this->view->assign('actionMessages', $actionMessages);
+
+		$this->view->assign('imageConfiguration', $this->getImageConfiguration());
 
 		return $this->view->render();
 	}
@@ -107,6 +129,143 @@ class TestSetup extends AbstractAction implements ActionInterface {
 		$message = $this->objectManager->get('TYPO3\\CMS\\Install\\Status\\OkStatus');
 		$message->setTitle('Created true type font dpi test image');
 		return $message;
+	}
+
+	/**
+	 * Create jpg from various image formats using IM / GM
+	 *
+	 * @return array Test results
+	 */
+	protected function convertImageFormatsToJpg() {
+		$this->setUpDatabaseConnectionMock();
+		$imageProcessor = $this->initializeImageProcessor();
+
+		$inputFormatsToTest = array(
+			'jpg',
+			'gif',
+			'png',
+			'tif',
+			'bmp',
+			'pcx',
+			'tga',
+			'pdf',
+			'ai',
+		);
+
+		$testResults = array();
+		$parseTimeStart = GeneralUtility::milliseconds();
+		foreach ($inputFormatsToTest as $formatToTest) {
+			$result = array();
+			if (!GeneralUtility::inList($GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'], $formatToTest)) {
+				/** @var \TYPO3\CMS\Install\Status\StatusInterface $message */
+				$message = $this->objectManager->get('TYPO3\\CMS\\Install\\Status\\WarningStatus');
+				$message->setTitle('Skipped test');
+				$message->setMessage(
+					'Handling format ' . $formatToTest . ' must be'
+					. ' enabled in TYPO3_CONF_VARS[\'GFX\'][\'imagefile_ext\']'
+				);
+				$result['error'] = $message;
+			} else {
+				$imageProcessor->IM_commands = array();
+
+				$inputFile = $this->imageBasePath . 'TestInput/Test.' . $formatToTest;
+				$imageProcessor->imageMagickConvert_forceFileNameBody = 'read-' . $formatToTest;
+				$imResult = $imageProcessor->imageMagickConvert($inputFile, 'jpg', '170', '', '', '', array(), TRUE);
+
+
+				$result['format'] = $formatToTest;
+				$result['outputFile'] = $imResult[3];
+				$result['referenceFile'] = $this->imageBasePath . 'TestReference/Read-' . $formatToTest . '.jpg';
+				$result['command'] = $imageProcessor->IM_commands;
+			}
+			$testResults[] = $result;
+		}
+		$parseTimeEnd = GeneralUtility::milliseconds();
+		$parseTime = $parseTimeEnd - $parseTimeStart;
+
+		$this->view->assign('testResults', $testResults);
+
+		/** @var \TYPO3\CMS\Install\Status\StatusInterface $message */
+		$message = $this->objectManager->get('TYPO3\\CMS\\Install\\Status\\OkStatus');
+		$message->setTitle('Executed test image formats tests');
+		$message->setMessage('Parse time: ' . $parseTime . ' ms');
+
+		return $message;
+	}
+
+	/**
+	 * Gather image configuration overview
+	 *
+	 * @return array Result array
+	 */
+	protected function getImageConfiguration() {
+		$result = array();
+		$result['imageMagickOrGraphicsMagick'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_version_5'] === 'gm' ? 'gm' : 'im';
+		$result['imageMagickEnabled'] =  $GLOBALS['TYPO3_CONF_VARS']['GFX']['im'];
+		$result['imageMagickPath'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_path'];
+		$result['imageMagickVersion'] = $this->determineImageMagickVersion();
+		$result['imageMagick5Effecs'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_v5effects'];
+		$result['imageMagickMaskInvert'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_imvMaskState'];
+		$result['gdlibEnabled'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['gdlib'];
+		$result['gdlibPng'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['gdlib_png'];
+		$result['freeTypeDpi'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['TTFdpi'];
+		$result['fileFormats'] = $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'];
+		return $result;
+	}
+
+	/**
+	 * Initialize image processor
+	 *
+	 * @return \TYPO3\CMS\Core\Imaging\GraphicalFunctions Initialized image processor
+	 */
+	protected function initializeImageProcessor() {
+		/** @var \TYPO3\CMS\Core\Imaging\GraphicalFunctions $imageProcessor */
+		$imageProcessor = $this->objectManager->get('TYPO3\\CMS\\Core\\Imaging\\GraphicalFunctions');
+		$imageProcessor->init();
+		$imageProcessor->tempPath = PATH_site . 'typo3temp/';
+		$imageProcessor->dontCheckForExistingTempFile = 1;
+		$imageProcessor->enable_typo3temp_db_tracking = 0;
+		$imageProcessor->filenamePrefix = 'installTool-';
+		$imageProcessor->dontCompress = 1;
+		$imageProcessor->alternativeOutputKey = 'typo3InstallTest';
+		$imageProcessor->noFramePrepended = $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_noFramePrepended'];
+		return $imageProcessor;
+	}
+
+	/**
+	 * Find out if ImageMagick or GraphicsMagick is enabled and set up
+	 *
+	 * @return bool TRUE if enabled and path is set
+	 */
+	protected function isImageMagickEnabledAndConfigured() {
+		$enabled = $GLOBALS['TYPO3_CONF_VARS']['GFX']['im'];
+		$path = $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_path'];
+		return $enabled && $path;
+	}
+
+	/**
+	 * Determine ImageMagick / GraphicsMagick version
+	 *
+	 * @return string Version
+	 */
+	protected function determineImageMagickVersion() {
+		$command = \TYPO3\CMS\Core\Utility\CommandUtility::imageMagickCommand('identify', '-version');
+		\TYPO3\CMS\Core\Utility\CommandUtility::exec($command, $result);
+		$string = $result[0];
+		list(, $version) = explode('Magick', $string);
+		list($version) = explode(' ', trim($version));
+		return trim($version);
+	}
+
+	/**
+	 * Instantiate a dummy instance for $GLOBALS['TYPO3_DB'] to
+	 * prevent real database calls
+	 *
+	 * @return void
+	 */
+	protected function setUpDatabaseConnectionMock() {
+		$database = $this->objectManager->get('TYPO3\\CMS\\Install\\Database\\DatabaseConnectionMock');
+		$GLOBALS['TYPO3_DB'] = $database;
 	}
 }
 ?>
