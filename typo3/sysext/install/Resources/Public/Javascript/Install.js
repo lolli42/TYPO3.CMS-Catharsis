96f3b33cf503f3325fd65dd556891a29dc0e2062
Christian Kuhn <lolli@schwarzbu.ch>
2013-06-16 12:53:23 +0200

    [TASK][INSTALL] Refactor password strength js to jquery and use in tool

--- typo3/sysext/install/Resources/Public/Javascript/Install.js
+++ typo3/sysext/install/Resources/Public/Javascript/Install.js
@@ -41,8 +41,29 @@ $(document).ready(function() {
 		$(this).closest('fieldset').find(':checkbox').prop('checked', this.checked);
 	});
 
+	// Toggle open/close
 	$('.toggleButton').on('click', function() {
 		$(this).closest('.toggleGroup').find('.toggleData').toggle();
+	});
+
+	// Simple password strength indicator
+	$('.t3-install-form-password-strength').on('keyup', function() {
+		var value = $(this).val();
+		var strongRegex = new RegExp('^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$', 'g');
+		var mediumRegex = new RegExp('^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$', 'g');
+		var enoughRegex = new RegExp('(?=.{6,}).*', 'g');
+
+		if (value.length == 0) {
+			$(this).attr('style', 'background-color:#FBB19B; border:1px solid #DC4C42');
+		} else if (false == enoughRegex.test(value)) {
+			$(this).attr('style', 'background-color:#FBB19B; border:1px solid #DC4C42');
+		} else if (strongRegex.test(value)) {
+			$(this).attr('style', 'background-color:#CDEACA; border:1px solid #58B548');
+		} else if (mediumRegex.test(value)) {
+			$(this).attr('style', 'background-color:#FBFFB3; border:1px solid #C4B70D');
+		} else {
+			$(this).attr('style', 'background-color:#FBFFB3; border:1px solid #C4B70D');
+		}
 	});
 
 });
