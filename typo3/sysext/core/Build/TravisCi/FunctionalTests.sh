6ff841a84112b23a1fc6636b12efb95b6630a767
Christian Kuhn <lolli@schwarzbu.ch>
2015-02-21 18:11:10 +0100

    [TRAVIS] Test

--- typo3/sysext/core/Build/TravisCi/FunctionalTests.sh
+++ typo3/sysext/core/Build/TravisCi/FunctionalTests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Running functional test suite ${1}"
 ./bin/phpunit --colors -c typo3/sysext/core/Build/FunctionalTests.xml ${1}
+RETURN=$?
 DB=`cat /tmp/${PARALLEL_SEQ}`
 mysql -u ${typo3DatabaseUsername} --password=${typo3DatabasePassword} --socket=${typo3DatabaseSocket} -e "DROP DATABASE ${DB}"
+exit ${RETURN}
