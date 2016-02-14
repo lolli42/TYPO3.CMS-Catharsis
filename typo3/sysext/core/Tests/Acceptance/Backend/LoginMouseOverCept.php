3deb85930293c520fb0f0fc6b801d2bd9b9267ce
Christian Kuhn <lolli@schwarzbu.ch>
2016-02-14 17:19:51 +0100

    travis-ci acceptance test 17

--- typo3/sysext/core/Tests/Acceptance/Backend/LoginMouseOverCept.php
+++ typo3/sysext/core/Tests/Acceptance/Backend/LoginMouseOverCept.php
@@ -5,6 +5,8 @@
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('check login functions');
+var_dump(file_get_contents('http://localhost:8000/klaus.php')); 
+/**
 $I->amOnPage('/typo3/index.php');
 $I->waitForElement('#t3-username');
 $I->wantTo('mouse over css change login button');
@@ -19,3 +21,4 @@ $bsmo = $I->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDrive
     return $webdriver->findElement(WebDriverBy::cssSelector('#t3-login-submit'))->getCSSValue('box-shadow');
 });
 $this->assertFalse($bs == $bsmo);
+ */
