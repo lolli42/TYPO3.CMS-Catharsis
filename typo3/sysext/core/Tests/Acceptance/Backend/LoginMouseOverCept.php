4271c6d12f1c68568bf5e292f65902accd3941e1
Christian Kuhn <lolli@schwarzbu.ch>
2016-02-14 23:04:27 +0100

    Revert "travis-ci acceptance test 18"
    This reverts commit d37607ad54c9f6adcce0ee2c85eb45a5ab6daa69.

--- typo3/sysext/core/Tests/Acceptance/Backend/LoginMouseOverCept.php
+++ typo3/sysext/core/Tests/Acceptance/Backend/LoginMouseOverCept.php
@@ -5,9 +5,10 @@
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('check login functions');
+var_dump(file_get_contents('http://localhost:8000/klaus.php')); 
+/**
 $I->amOnPage('/typo3/index.php');
-$I->wait(10);
-$I->waitForElement('#t3-username', 10);
+$I->waitForElement('#t3-username');
 $I->wantTo('mouse over css change login button');
 
 $bs = $I->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDriver $webdriver) {
@@ -20,3 +21,4 @@ $bsmo = $I->executeInSelenium(function(\Facebook\WebDriver\Remote\RemoteWebDrive
     return $webdriver->findElement(WebDriverBy::cssSelector('#t3-login-submit'))->getCSSValue('box-shadow');
 });
 $this->assertFalse($bs == $bsmo);
+ */
