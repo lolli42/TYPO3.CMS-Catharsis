e40eb4a95f82d5ba2ecd6fec5b9cde6e4061d311
Christian Kuhn <lolli@schwarzbu.ch>
2016-04-20 17:11:35 +0200

    nginx 8
    Change-Id: Ied843ae3d78cd4ad4216047b497b85e5dde30a46

--- typo3/sysext/core/Build/Configuration/TravisNginxFpm/ConfigureStartNginxFpm.sh
+++ typo3/sysext/core/Build/Configuration/TravisNginxFpm/ConfigureStartNginxFpm.sh
@@ -7,7 +7,7 @@ DIR=$(realpath $(dirname "$0"))
 USER=$(whoami)
 PHP_VERSION=$(phpenv version-name)
 ROOT=$(realpath "$DIR/../../../../../..")
-PORT=9000
+FPMPORT=9000
 SERVER="/tmp/php.sock"
 
 function tpl {
@@ -16,7 +16,7 @@ function tpl {
         -e "s|{USER}|$USER|g" \
         -e "s|{PHP_VERSION}|$PHP_VERSION|g" \
         -e "s|{ROOT}|$ROOT|g" \
-        -e "s|{PORT}|$PORT|g" \
+        -e "s|{FPMPORT}|$FPMPORT|g" \
         -e "s|{SERVER}|$SERVER|g" \
         < $1 > $2
 }
